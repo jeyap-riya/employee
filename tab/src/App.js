c089e13e41028f51e7828676c3ca63fc0bccfd80
Jeyapriya <jeyapriya8807@gmail.com>
2023-03-23 15:22:36 +0530

    i change add poup changes to edit changes to delete changes to js file

--- tab/src/App.js
+++ tab/src/App.js
@@ -1,70 +1,81 @@
-import './App.css';
 import { useState } from 'react';
-import {Button, Table,Modal,Input,Space,} from "antd";
+import { Table,Modal,Input,Space,Button,Form} from "antd";
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
-import Select from "react-select"
+import * as React from 'react';
+
+
 
 
 
 
 function App() {
-  const [isEditing, setIsEditing] = useState(false)
-  const[editingStudent,setEditingStudent] = useState(null)
+  const [isEdit, setIsEdit] = useState(false)
+  const[edit,setEdit] = useState(null)
+ 
   const [dataSource, setDataSource] = useState([
     {
       id:1,
-      fname:'john',
-      lname:'marry',
+      firstName:'john',
+      lastName:'marry',
       email:'john@gmail.com',
       gender:'female',
-      dob:'18/5/1990',
-      dateofjoining:'6/6/2015',
+      dateOfBirth:'2023-03-14',
+      dateOfJoining:'2023-03-14',
     },
     {
       id:2,
-      fname:'anthony',
-      lname:'stanlin',
+      firstName:'anthony',
+      lastName:'stanlin',
       email:'anthontstalin@gmail.com',
       gender:'male',
-      dob:'13/11/2000',
-      dateofjoining:'10/10/22',
+      dateOfBirth:'2023-03-14',
+      dateOfJoining:'2023-03-14',
     },
     {
       id:3,
-      fname:'jeya',
-      lname:'malai',
+      firstName:'jeya',
+      lastName:'malai',
       email:'jeyamalai@gmail.com',
       gender:'male',
-      dob:'25/5/1994',
-      dateofjoining:'13/10/22',
+      dateOfBirth:'2023-03-14',
+      dateOfJoining:'2023-03-14',
     },
     {
       id:4,
-      fname:'pandi',
-      lname:'selvi',
+      firstName:'pandi',
+      lastName:'selvi',
       email:'pandiselvi@gmail.com',
       gender:'female',
-      dob:'28/5/1999',
-      dateofjoining:'18/10/2023',
+      dateOfBirth:'2023-03-14',
+      dateOfJoining:'2023-03-14',
+    },
+    {
+      id:5,
+      firstName:'pandi',
+      lastName:'selvi',
+      email:'pandiselvi@gmail.com',
+      gender:'female',
+      dateOfBirth:'2023-03-14',
+      dateOfJoining:'2023-03-14',
     },
     
     {
       id:6,
-      fname:'jeya',
-      lname:'priya',
+      firstName:'jeya',
+      lastName:'priya',
       email:'jeyapriya@gmail.com',
       gender:'female',
-      dob:'06/10/2000',
-      dateofjoining:'1/3/23',
+      dateOfBirth:'2023-03-14',
+      dateOfJoining:'2023-03-14',
     },
     {
       id:7,
-      fname:'nithish',
-      lname:'pushba',
+      firstName:'nithish',
+      lastName:'pushba',
       email:'nithishpushba@gmail.com',
       gender:'male',
-      dob:'18/5/2006',
-      dateofjoining:'10/10/23',
+      dateOfBirth:'2023-03-14',
+      dateOfJoining:'2023-03-14',
     },
 
     
@@ -78,12 +89,12 @@ function App() {
     {
       key:'2',
       title:'First Name',
-      dataIndex:'fname',
+      dataIndex:'firstName',
     },
     {
       key:'3',
       title:'Last Name',
-      dataIndex:'lname',
+      dataIndex:'lastName',
     },
     {
       key:'4',
@@ -99,12 +110,12 @@ function App() {
     {
       key:'6',
       title:'Date of Birth',
-      dataIndex:'dob',
+      dataIndex:'dateOfBirth',
     },
     {
       key:'7',
       title:'Date of joining',
-      dataIndex:'dateofjoining',
+      dataIndex:'dateOfJoining' ,
     },
     {
       key:'8',
@@ -114,158 +125,162 @@ function App() {
         <>
         
         <EditOutlined onClick={()=>{
-          onEditStudent(record)
+          onEdit(record)
           
            }}style={{color:"blue",marginLeft:12}}/>
         <DeleteOutlined onClick={()=>{
-         onDeleteStudent(record)
+         onDelete(record)
         }} style={{color:"red",marginLeft:12}}/>
-        
+       
+  
         </>
         );
       },
     },
-    
-  ];
-
-  const onAddStudent = () => {
-    const randomstring = parseInt(Math.random() * 1000);
-    const newStudent = {
-      id: randomstring,
-      fname:' '+randomstring,
-      lname:' '+randomstring,
-      gender:'male',
-      email: randomstring+"@gmail.com",
-     dob:' '+randomstring,
-      dateofjoining:' '+randomstring,
-      
-    };
-    setDataSource((pre) => {
-      return[...pre, newStudent]
-    });
-
-  };
-  const onDeleteStudent=(record)=>{
+  ]
+    const onDelete=(record)=>{
     Modal.confirm({
       title:'Are you sure,you want to delete this employee record?',
       okText:'Yes',
       okType:'danger',
       onOk: () => {
         setDataSource((pre) => {
-          return pre.filter((student) => student.id !== record.id);
+          return pre.filter((employee) => employee.id !== record.id);
          });
       },
     });
        
   };
-  const onEditStudent = (record) =>{
-    setIsEditing(true);
-    setEditingStudent({...record})
+  const onEdit = (record) =>{
+    setIsEdit(true);
+    setEdit({...record})
     
    
   };
-  const resetEditing=()=>{
-    setIsEditing(false);
-    setEditingStudent(null);
+  const resetEdit=()=>{
+    setIsEdit(false);
+    setEdit(null);
     
   };
-  var list=[
-    {
-      value:1,
-      label:"male"
-    },
-    {
-      value:2,
-      label:"female"
-    },
-    {
-      value:3,
-      label:"other"
-    }
-  ];
   
-  return (
+  const onAdd = () => {
+    const newEmployee ={
+      id:'',
+      firstName:'',
+      lastName:'',
+      email:'',
+      gender:'',
+      dateOfBirth:'',
+      dateOfJoining:'', 
+    }
+    setDataSource(pre=>{
+      return[...pre, newEmployee ];
+    }); 
+  };
+   
+   return (
+
     <div className="App">
       <header className="App-header">
-      <h1>Employee Details</h1> <br/><br/>
-      <Button onClick={onAddStudent} style={{backgroundColor:"skyblue", color:"Black"}}>Add new employee</Button><br/><br/>
-     <Table columns={columns} dataSource={dataSource} />
-        <Modal
-           title="Edit Employee"
-           visible={isEditing} 
-           okText="Save"
-           onCancel={()=>{
-            resetEditing()
+      <center><h1>Employee Details</h1> </center><br/><br/>
+      <div><center><Button  onClick={onAdd} style={{backgroundColor:"skyblue", color:"Black"}}>Add new employee</Button><br/><br/></center></div>
+       <div><Table columns={columns} dataSource={dataSource} /></div>
+       <div><Modal
+       title="Edit Employee"
+       visible={isEdit}
+       okText="Save"
+       onCancel={()=>{
+          resetEdit()
            }}
            
-          onOk={() => {
-            setDataSource(pre=>{
-               return pre.map(student=>{
-                if(student.id === editingStudent.id){
-                  return editingStudent
+      onOk={() => {
+        setDataSource(pre=>{
+          return pre.map(employee=>{
+              if(employee.id === edit.id){
+                 return edit
                 }
-                else{
-                  return student;
+            
+            
+            else{
+                 return employee;
                 }
-               })
-            })
-             resetEditing()
-          }  }                                               
+               });
+            });
+             resetEdit()
+          }} 
+       ><Form
+        autoComplete="off" 
+        labelCol={{ span:10}} 
+        wrapperCol={{ span:10}}
+        onFinish={( values) =>{
+          console.log({ values});
+        }}
+        onFinishFailed={(error) => {
+          console.log({ error });
+        }} 
         >
           <Space direction="vertical" style={{ width: '100%' }}>
-          <Input   value={editingStudent?.fname} onChange={(e)=>{
-            setEditingStudent(pre=>{
-              return{...pre,fname:e.target.value}
-            })
-          }}
-            />
-             <Input   value={editingStudent?.lname} onChange={(e)=>{
-            setEditingStudent(pre=>{
-              return{...pre,lname:e.target.value}
-            })
-          }}
-            />
-          <Input   value={editingStudent?.email} onChange={(e)=>{
-            setEditingStudent(pre=>{
-              return{...pre,email:e.target.value}
-            })
-          }}/>
-          
-          <Select options= {list} style={{ width: 470  }} 
-          placeholder="Gender"
-        value={editingStudent?.gender} onChange={(e)=>{
-            setEditingStudent(pre=>{
-              return{...pre,gender:e.target.value}
-            })
-          }}/>
-            
-        <Input type='date' style={{ width: 470  }} value={editingStudent?.dob} onChange={(e)=>{
-            setEditingStudent(pre=>{
-              return{...pre,dob:e.target.value}
-            });
-          }}
-              placeholder='Date of Birth' />
-     
-       
            
-            <Input type="date"style={{ width: 470  }}   value={editingStudent?.dateofjoining} onChange={(e)=>{
-            setEditingStudent(pre=>{
-              return{...pre,dateofjoining:e.target.value}
-            });
-          }}
-              placeholder='Date of joining'    />
-      
-            
-           
-            
-          </Space>  
-             
+            <Form.Item name="firstName"label="First Name" 
+            rules={[
+              {
+                required: true,
+                message: "please Enter your First name",
+                },
+                {whitespace:true},
+                {min: 3},
+                ]}hasFeedback>  
+              <Input value={edit?.firstName} onChange={(e) => {  setEdit((pre)=>{  return{...pre, firstName: e.target.value}; }); }} placeholder="firstName"></Input></Form.Item>
+            <Form.Item name="lastName"label= "Last Name"  rules={[
+              {
+                required: true,
+                message: "please Enter your First name",
+                },
+                {whitespace:true},
+                {min: 3},
+                ]}hasFeedback> 
+             <Input value={edit?.lasttName} onChange={(e) => {  setEdit((pre)=>{  return{...pre, lastName: e.target.value}; }); }} placeholder="lastName"></Input></Form.Item>
+             <Form.Item name="email"label= "Email"  rules={[
+              {
+                required: true,
+                message: "please Enter your Email",
+                },
+                {type: "email"},
+                ]}>
+              <Input value={edit?.email} onChange={(e) => {  setEdit((pre)=>{  return{...pre, email: e.target.value}; }); }} placeholder="email"></Input></Form.Item>
+              <Form.Item name="gender"label= "Gender" requiredMark="optional"> 
+              <select  placeholder="gender"  style={{ color:"gray", width: '100%',height:"33px" }} value={edit?.gender} onChange={(e) => {setEdit((pre)=>{return{...pre, gender: e.target.value};});}} >
+                      <option value="Male">male</option>
+                      <option value="female">female</option>
+                     <option value="other">other</option>
+              </select></Form.Item>
+            <Form.Item name="dateOfBirth"label= "date Of Birth"  rules={[
+              {
+                required: true,
+                message: "please provide your date of birth",
+                },
+               
+                ]}hasFeedback>
+              <Input type="date" value={edit?.dateOfBirth} onChange={(e) => {  setEdit((pre)=>{  return{...pre, dateOfBirth: e.target.value}; }); }} placeholder="dateOfBirth"></Input></Form.Item>
+            <Form.Item name="date of joing"label= "date Of Joining"  rules={[
+              {
+                required: true,
+                message: "please provide your joining date",
+                },
+                
+                ]}hasFeedback>
+              <Input type="date" value={edit?.dateOfJoining} onChange={(e) => {  setEdit((pre)=>{  return{...pre, dateOfJoining: e.target.value}; }); }} placeholder="dateOfJoining"></Input></Form.Item>
+              
+          </Space> 
+          </Form> 
         </Modal>
-        
+        </div>
       </header>
     </div>
-  );
+    
+   
+   );
 }
-
+  
 export default App;
 
